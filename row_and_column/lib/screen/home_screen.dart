517a362d2d559abb61eed34ae8f0083057365971
yoon <envy15@skuniv.ac.kr>
2024-01-06 21:49:40 +0900

    Row_and_Column 실습 프로젝트 완성

---
+++ row_and_column/lib/screen/home_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    color: Colors.orange,
+                  ),
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    color: Colors.orange,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    color: Colors.orange,
+                  ),
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
