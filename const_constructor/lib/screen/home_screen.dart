6e4365ecea726c49ff737680d977c802526c7eeb
yoon <envy15@skuniv.ac.kr>
2024-01-11 14:48:08 +0900

    const_constructor 예제 생성

---
+++ const_constructor/lib/screen/home_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TestWidget(label: 'label1'),
+            const TestWidget(label: 'label2'),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {});
+              },
+              child: const Text(
+                '빌드!',
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TestWidget extends StatelessWidget {
+  final String label;
+
+  const TestWidget({
+    required this.label,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    print('$label 빌드 실행');
+    return Container(
+      child: Text(
+        label,
+      ),
+    );
+  }
+}
